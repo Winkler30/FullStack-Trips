2901212939f2912d59a4c2794d585e2382cfc33f
Winkler30 <charleswinkler76@gmail.com>
2023-09-27 21:45:08 -0300

    feat: initial components

--- src/app/about/components/Trips.tsx
+++ src/app/about/components/Trips.tsx
@@ -1,7 +1,23 @@
+import { prisma } from "@/lib/prisma";
 import React, { useState } from "react";
 
-const Trips = () => {
-  return <div>Trips</div>;
+const getTrips = async () => {
+  const trips = await prisma.trip.findMany();
+  return trips;
+};
+
+const Trips = async () => {
+  const data = await fetch("http://jsonplaceholder.typicode.com/posts").then(
+    (res) => res.json()
+  );
+
+  return (
+    <div>
+      {data.map((i : any) => (
+        <p key={i.id}>{i.title}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Trips;
