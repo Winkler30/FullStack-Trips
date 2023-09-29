dc26f6d2940c4cea41f8a5bb21eca822e179748d
Winkler30 <charleswinkler76@gmail.com>
2023-09-29 19:14:30 -0300

    feat: update database schema

---
+++ prisma/migrations/20230929221226_auth/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - Added the required column `countryCode` to the `Trip` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "countryCode" TEXT NOT NULL,
+ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+ADD COLUMN     "recommended" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "TripReservation" ADD COLUMN     "guests" INTEGER NOT NULL DEFAULT 5;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
 export default function Home() {
-  return <h1>Home page</h1>
+  return <h1>Home page</h1>;
 }
