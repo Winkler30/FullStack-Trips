b1312af1b4f3806dcd6acd011d92a1c9e4660b82
Winkler30 <charleswinkler76@gmail.com>
2023-10-20 05:38:38 -0300

    feat: add trip buy

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -10,6 +10,7 @@ import ptBR from "date-fns/locale/pt-BR";
 import Button from "@/components/Button";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   const [trip, setTrip] = useState<Trip | null>();
@@ -17,7 +18,7 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
 
   const router = useRouter();
 
-  const {status} =  useSession()
+  const { status, data } = useSession();
 
   const searchParams = useSearchParams();
 
@@ -32,24 +33,52 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
         }),
       });
 
-      const res = await response.json()
+      const res = await response.json();
 
-      if (res?.error){
-        return router.push("/")
+      if (res?.error) {
+        return router.push("/");
       }
 
       setTrip(res.trip);
       setTotalPrice(res.totalPrice);
     };
 
-    if (status === "unauthenticated"){
-      router.push("/")
+    if (status === "unauthenticated") {
+      router.push("/");
     }
 
     fetchTrip();
   }, [status, searchParams, params, router]);
 
   if (!trip) return null;
+
+  const handleBuyClick = async () => {
+    const res = await fetch("http://localhost:3000/api/trips/reservation", {
+      method: "POST",
+      body: Buffer.from(
+        JSON.stringify({
+          tripId: params.tripId,
+          startDate: searchParams.get("startDate"),
+          endDate: searchParams.get("endDate"),
+          guests: Number(searchParams.get("guests")),
+          userId: (data?.user as any)?.id!,
+          totalPaid: totalPrice,
+        })
+      ),
+    });
+
+    if (!res.ok) {
+      return toast.error("Erro ao realizar reserva!", {
+        position: "bottom-center",
+      });
+    }
+
+    router.push("/");
+
+    toast.success("Reserva realizada com sucesso!", {
+      position: "bottom-center",
+    });
+  };
 
   const startDate = new Date(searchParams.get("startDate") as string);
   const endDate = new Date(searchParams.get("endDate") as string);
@@ -103,8 +132,9 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
         <h3 className="font-semibold mt-3">Hóspedes</h3>
         <p>{guests} hóspedes</p>
 
-        <Button className="mt-5">Finalizar compra</Button>
-
+        <Button className="mt-5" onClick={handleBuyClick}>
+          Finalizar compra
+        </Button>
       </div>
     </div>
   );
