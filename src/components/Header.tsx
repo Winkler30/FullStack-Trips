d3fa353d44d167ec701035153e708b7b83c097b3
Winkler30 <charleswinkler76@gmail.com>
2023-10-20 05:50:32 -0300

    feat: add my trips button to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 
 const Header = () => {
   const [menuIsOpen, setMenuIsOpen] = React.useState(false);
+
   const { status, data } = useSession();
 
   const handleLoginClick = () => signIn();
@@ -49,9 +50,16 @@ const Header = () => {
             className="rounded-full shadow-md"
           />
           {menuIsOpen && (
-            <div className="z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-sm flex flex-col justify-center items-center">
+            <div className="z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-sm flex flex-col justify-center items-center">
+              <Link href="/my-trips">
+                <button
+                  className="text-primary pb-2 border-b border-grayLighter border-solid text-sm font-semibold">
+                  Minhas viagens
+                </button>
+              </Link>
+
               <button
-                className="text-primary text-sm font-semibold"
+                className="text-primary pt-2  text-sm font-semibold"
                 onClick={handleLogoutClick}
               >
                 Logout
