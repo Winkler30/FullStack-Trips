1907d5a4e02fba91df347e33592393b0d5520ef9
Winkler30 <charleswinkler76@gmail.com>
2023-10-23 06:07:13 -0300

    fix

--- src/app/api/trips/reservation/[reservationId]/route.ts
+++ src/app/api/trips/reservation/[reservationId]/route.ts
@@ -1,21 +1,34 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { NextApiResponse } from "next";
 
-export async function DELETE(_request: Request, { params: { reservationId } }: { params: { reservationId: string } }) {
+export async function DELETE(_request: Request, { params: { reservationId } }: { params: { reservationId: string } }, _response: NextApiResponse) {
   if (!reservationId) {
-    return {
-      status: 400,
-      body: {
+    return new NextResponse(
+      JSON.stringify({
         message: "Missing reservationId",
-      },
-    };
+      }),
+      { status: 400 }
+    );
   }
 
-  const reservation = await prisma.tripReservation.delete({
-    where: {
-      id: reservationId,
-    },
-  });
+  try {
+    const reservation = await prisma.tripReservation.delete({
+      where: {
+        id: reservationId,
+      },
+    });
 
-  return new NextResponse(JSON.stringify(reservation), { status: 200 });
+    return new NextResponse(
+      JSON.stringify(reservation),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({
+        message: "Error deleting reservation",
+      }),
+      { status: 500 }
+    );
+  }
 }
